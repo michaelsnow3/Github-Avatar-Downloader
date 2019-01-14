507bea2de4e540cee4585b15f11649d80718001a
vagrant <mikesnow444@gmail.com>
2019-01-14 21:41:19 +0000

    added options object to add user agent and authorization headers

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,20 @@
 var request = require('request');
+var token = require('./secrets.js');
 
-console.log('Github Avatar Downloader');
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${token.GITHUB_TOKEN}`
+    }
+  }
+  request(options, function (err, res, body) {
+    cb(err, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
