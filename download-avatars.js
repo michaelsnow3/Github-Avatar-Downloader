76a330babd1a68a78d0ddcb095fc68f11b43ee80
vagrant <mikesnow444@gmail.com>
2019-01-14 23:32:36 +0000

    requires 2 parameters to be entered to attempt a request

--- download-avatars.js
+++ download-avatars.js
@@ -1,8 +1,15 @@
 var request = require('request');
 var token = require('./secrets.js');
 var fs = require('fs');
+var inputRepoOwner = process.argv[2];
+var inputRepoName = process.argv[3];
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  if(!repoOwner || !repoName){
+    console.log("enter repo owner and repo name");
+    return 0;
+  }
+  console.log('attempt a request');
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -12,7 +19,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
   request(options, function (err, res, body) {
     var data = JSON.parse(body);
-    // console.log(data);
     cb(err, data);
   });
 }
@@ -20,10 +26,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
 function downloadImageByURL(url, filePath) {
   request
     .get(url)
+    .on('error', function(err) {
+      throw err;
+    })
     .pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(inputRepoOwner, inputRepoName, function(err, result) {
   console.log("Errors:", err);
   result.forEach(function(contributor) {
     downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}.jpg`);
